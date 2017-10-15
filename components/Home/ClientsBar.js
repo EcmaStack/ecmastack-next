78624cbaba2cd8689dc8181a4db65cba4fb8fea4
Alex Bachuk <alex@bachuk.com>
2017-10-15 16:33:20 -0400

    styling clientsBar

--- components/Home/ClientsBar.js
+++ components/Home/ClientsBar.js
@@ -6,12 +6,20 @@ const ClientsBar = () => {
          <img src="static/bizlybw.svg" alt="Bizly" className="client-logo" />
          <img src="static/sloohbw.svg" alt="Slooh" className="client-logo" />
          <img src="static/giffybw.svg" alt="GiftFly" className="client-logo" />
-         <img src="static/classNamematebw.svg" alt="classNamemate" className="client-logo" />
+         <img src="static/classmatebw.svg" alt="classNamemate" className="client-logo" />
          <img src="static/minellibw.svg" alt="minelli" className="client-logo" />
        </div>
       </section>
      <style jsx>{`
+       .home-clients {
+         padding: 64px 0;
+         background: #212A3D;
+       }
 
+       .home-clients .client-logo {
+         max-height: 54px;
+         flex: 0 0 auto;
+       }
      `}</style>
      </div>
     )
